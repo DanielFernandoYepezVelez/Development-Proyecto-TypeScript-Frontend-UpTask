af6573b28b37a6de23e3d0ac32edb4328f951955
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-09-12 22:38:40 -0500

    Maquetando El Login Con CSS Desde Cero

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  private name: string;
+  private lastName: string;
+  private date: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    this.name = 'Daniel Fernando';
+    this.lastName = 'Yepez Vélez';
+    this.date = new Date().getFullYear();
+  }
+  
+  get Name() {
+    return this.name;
   }
 
+  get LastName() {
+    return this.lastName;
+  }
+
+  get Date() {
+    return this.date;
+  }
 }
