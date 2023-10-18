8b94c8845484b92668b5dc7cff3a2bf1ec4c8049
DanielFernandoYepezVelezPersonalSoft <dfyepes@personalsoft.com>
2023-10-18 13:51:49 -0500

    Cambiando El Ambiente A Produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://localhost:3000/api',
-  baseUrl: 'https://uptask-backend.herokuapp.com/api',
+  // baseUrl: 'https://uptask-backend.herokuapp.com/api',
+  baseUrl: 'https://uptask-administration.onrender.com/api'
 };
